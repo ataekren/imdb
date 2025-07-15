9409746071170749db78889de6761e464b5ef43f
ata <21070001065@stu.yasar.edu.tr>
2025-07-15 15:15:13 +0300

    Update environment.prod.ts

--- frontend/imdb-frontend/src/environments/environment.prod.ts
+++ frontend/imdb-frontend/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-url.com/api',
+  apiUrl: '${API_URL}',
   firebase: {
-    apiKey: "AIzaSyB1keTxZhgEcACMV9ri-9jpKND7qt4Gu8w",
-    authDomain: "imdb-fb.firebaseapp.com",
-    projectId: "imdb-fb",
-    storageBucket: "imdb-fb.firebasestorage.app",
-    messagingSenderId: "544038652356",
-    appId: "1:544038652356:web:9e4ad75c4f84ed15ac3c91"
+    apiKey: '${FIREBASE_API_KEY}',
+    authDomain: '${FIREBASE_AUTH_DOMAIN}',
+    projectId: '${FIREBASE_PROJECT_ID}',
+    storageBucket: '${FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${FIREBASE_APP_ID}'
   }
-}; 
+};
